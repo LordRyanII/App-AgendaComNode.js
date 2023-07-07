b724d64f0d68c1c2cfaa079506b2638d5089e239
LordRyanII <ryanoliveiram2020@outlook.com>
2023-07-06 23:36:06 -0300

    Criando usuário part1

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,14 +1,22 @@
+const { async } = require('regenerator-runtime');
 const Login = require('../models/loginModels');
 
 exports.index = (req, res) => {
     return res.render('login')
 };
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
     const login = new Login(req.body);
-    login.register()
+    await login.register();
 
-    //if(login.errors.length > 0)
+    if(login.errors.length > 0){
+        req.flash('errors', login.errors);
+        req.session.save(() => {
+            return req.redirect('back');
+        });
+        return;
+    }
+
 
     res.send(login.errors);
 }
--- src/models/loginModels.js
+++ src/models/loginModels.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator')
 
 const loginSchema = new mongoose.Schema({
-  email: {type: String, required: true},
-  senha: {type: String, required: true},
+  email: { type: String, required: true },
+  senha: { type: String, required: true },
 });
 
 const loginModel = mongoose.model('login', loginSchema);
@@ -14,27 +14,34 @@ class login {
     this.errors = [];
     this.user = null;
   }
-  register(){
+  async register() {
     this.valida();
-    if(this.errors.length > 0) return;
+    if (this.errors.length > 0) return;
+
+    try {
+      this.user = await loginModel.create(this.body)
+    } catch (error) {
+      console.log(error)
+    };
+    this.user = await loginModel.create(this.body)
   }
   valida() {
     this.cleanUp();
     //O email precisa ser valido
-    if(!validator.isEmail(this.body.email)) {
+    if (!validator.isEmail(this.body.email)) {
       this.errors.push('Email inválido');
     }
-    if(this.body.password.length < 3 || this.body.password.length >= 50){
+    if (this.body.password.length < 3 || this.body.password.length >= 50) {
       this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
     }
     //Senha entre 3 e 50 caracteres
 
 
   }
-  cleanUp(){
-    for(const key in this.body){
-      if(typeof this.body[key] !== 'string'){
-        this.body[key]= '';
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
       }
     }
     this.body = {
