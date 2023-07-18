97dc4eb86ea5a5587be1d3224462cbe53814d4a2
LordRyanII <ryanoliveiram2020@outlook.com>
2023-07-17 23:15:27 -0300

    Adicionando estilos

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -19,3 +19,13 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user){
+    req.flash('errors', 'Você precisa fazer login. Tente novamente.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
+  next()
+}
